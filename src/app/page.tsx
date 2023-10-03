7c5be11936cb9efa8592d67b52e15bebe92de547
Ahmedarshad14 <meahmed1@hotmail.com>
2023-10-04 03:08:33 +0500

    Added Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Image from 'next/image'
+import Navbar from '@/components/Navbar'
 
 export default function Home() {
   return (
     <>
+    <Navbar />
       <div className='flex my-auto mx-auto h-[100vh]'>
         <h1 className='text-gray-600 text-center text-8xl my-auto mx-auto'>Hello, World!</h1>
       </div>
